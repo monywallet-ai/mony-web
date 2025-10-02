ef79f1e4f599b7e768367b323f32d4091d256fea
Camilo Vargas <ca06vargas@gmail.com>
2025-10-02 00:22:36 -0500

    Add Pinia for state management and implement a simple counter feature
    - Updated package.json and bun.lock to include Pinia as a dependency.
    - Modified main.ts to create and use a Pinia store.
    - Created a new counter store in src/store/counter.ts with increment and decrement actions.
    - Enhanced Home.vue to display a simple counter with increment and decrement buttons.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,63 @@
 <script setup lang="ts">
+import { useCounterStore } from '@/store/counter'
+import { computed } from 'vue';
+
+const counterStore = useCounterStore()
+
+const isZero = computed(() => {
+    return counterStore.count === 0
+})
+
+
 </script>
 
 <template>
-    <h1>Home</h1>
+    <div>
+        <h1>Simple counter</h1>
+        <p>Count is: {{ counterStore.count }}</p>
+        <button @click="counterStore.increment">Increment</button>
+        <button v-if="!isZero" @click="counterStore.decrement">
+            Decrement
+        </button>
+    </div>
 </template>
+
+<style scoped>
+div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+
+    &>button {
+        margin-top: 20px;
+    }
+}
+
+h1 {
+    font-weight: 500;
+    font-size: 2.6rem;
+    top: -10px;
+}
+
+p {
+    font-size: 1.5rem;
+}
+
+button {
+    font-size: 1.25rem;
+    font-weight: 500;
+    background-color: #4caf50;
+    color: white;
+    padding: 10px 20px;
+    border-radius: 5px;
+    border: none;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+
+    &:hover {
+        background-color: #3e8e41;
+    }
+}
+</style>
